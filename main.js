bbca33be78c444e8151608062c3c10272aa15ae4
Steven Rutherford <steve.rutherford12@gmail.com>
2019-06-23 19:24:25 +0100

    Add website basics

--- main.js
+++
@@ -1,6 +0,0 @@
-var GAME_SCREEN_WIDTH = 800;
-var GAME_SCREEN_HEIGHT = 600;
-
-Crafty.init(GAME_SCREEN_WIDTH,GAME_SCREEN_HEIGHT, document.getElementById('game'));
-
-Crafty.scene("Loading");
--- scenes/game.js
+++
@@ -1,7 +0,0 @@
-Crafty.defineScene("Game", function() {
-	
-	var redSquare = Crafty.e('2D, DOM, Color')
-		.attr({x: 0, y: 0, w: 100, h: 100})
-		.color('#F00');
-
-});
